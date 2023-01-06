afe3522be20b197485a786963f7001120ba85475
data-in-playdata <jewoos15@naver.com>
2023-01-06 13:59:48 +0900

    modify 'get aliquot' part

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.py
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -2,11 +2,43 @@ arr = []
 N, K = map(int, input().split())
 cont = 0
 for i in range(1, N + 1):
-   if N % i ==0:
+   if N % i == 0:
       arr.append(i)
-   cont += 1
+   cont += 1 # cont는 약수의 개수
           
-if K > len(arr):
+if K > len(arr): 
    print(0)
 else:
     print(arr[K-1])
+
+'''
+약수의 개수 구하기
+N값이 주어지면 N값의 약수의 개수를 구하시오.
+'''
+import math
+N = int(input())
+cnt = 0
+for i in range(1, int(math.sqrt(N)) + 1):
+    if N == i * i:
+        cnt += 1
+        continue
+    if N % i == 0:
+        cnt += 2
+
+print(cnt)
+ """
+ 혹은
+ """
+ N = int(input())
+cnt = 0
+i = 1
+while i * i < N:
+    if N % i == 0:
+        cnt += 2
+    i += 1
+if N == i * i:
+    cnt += 1
+
+
+print(cnt)
+
