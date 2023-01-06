d75387c3b39146900793d91e5c9242f9ad218e6e
Jewoo <105477856+jewoodev@users.noreply.github.com>
2023-01-06 16:41:22 +0900

    Update 이진수.py

--- 백준/Bronze/3460. 이진수/이진수.py
+++ 백준/Bronze/3460. 이진수/이진수.py
@@ -1,6 +1,6 @@
 T = int(input())
 for i in range(T):
-    n = bin(int(input()))[2:] # bin함수는 10진수 숫자를 2진수 문자열로 바꾼다. 문자열임에 주목!
+    n = bin(int(input()))[2:] # bin함수는 10진수 숫자를 2진수 문자열로 바꾼다. 반환값이 문자열임에 주목!
     for idx, val in enumerate(n[::-1]):
         if val == '1':
             print(idx, end = ' ')
