2109b7c7a4b19d30873e9f0e19e8ee09c7dfa054
Jewoo <105477856+jewoodev@users.noreply.github.com>
2023-01-12 20:10:07 +0900

    Add solution use recursion function

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -3,3 +3,16 @@ nbh = int(input())
 for i in range(nbh):
     nlist.append(nlist[i] + nlist[i+1])
 print(nlist[nbh])
+
+--------------------------------------------------------------------------------
+
+"""
+다른 풀이 - 재귀 함수
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+n = int(input())
+print(fibonacci(n))
+"""
