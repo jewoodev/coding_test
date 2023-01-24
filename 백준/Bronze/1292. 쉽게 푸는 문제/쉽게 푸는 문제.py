4a755077ef4ee169c4393742a9738970f2df782f
jewoodev <jewoos15@naver.com>
2023-01-24 21:34:56 +0900

    add the things I studied on January 24.

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
@@ -9,3 +9,57 @@ sum_result = 0
 for i in range(a-1, b):
     sum_result += progression[i]
 print(sum_result)
+
+### 다른 풀이 ###
+
+"""
+a, b = map(int, input().split())
+ 
+arr = [0]
+cnt = 1
+for i in range(1, (b+1) // 2 + 1):    # 범위를 반으로 나눠서 시간 단축
+    for j in range(cnt):
+        arr.append(cnt)
+    cnt += 1
+ 
+for i in range(1, b+1):
+    arr[i] += arr[i-1]
+print(arr[b] - arr[a-1])
+
+"""
+
+"""
+a,b = map(int,input().split())
+ 
+arr = [0]
+for i in range(46):
+    for j in range(i):
+        arr.append(i)
+ 
+print(sum(arr[a:b+1]))
+"""
+
+"""
+import sys
+
+a, b = map(int, sys.stdin.readline().split())
+temp = []
+# 반복문을 통해 b구간까지의 수열을 만든다.
+for i in range(1, b + 1):
+    for j in range(i):
+        temp.append(i)
+
+# 만든 수열의 a부터 b구간까지 더한 값을 출력
+print(sum(temp[a - 1:b]))
+"""
+
+"""
+x, y = map(int, input().split())
+a = []
+for i in range(y+1) :
+    for j in range(i):
+        if len(a) == y :
+            break
+        a.append(i)
+print(sum(a[x-1:y]))
+"""
--- 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.py
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.py
@@ -3,3 +3,10 @@ for i in range(T):
     input_val = list(map(int, input().split()))
     input_val.sort()
     print(input_val[-3])
+    
+
+    
+"""
+import sys
+input = sys.stdin.readline      # 이 모듈을 이용해서 메모리 사용을 효율적으로 할 수 있다.
+"""
