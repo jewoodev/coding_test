b60242c867d9be684b2dd5bfd6aca25c42e8f867
jewoodev <jewoos15@naver.com>
2023-01-26 23:35:43 +0900

    recall concubine

--- 백준/pratice.py
+++ 백준/pratice.py
@@ -1,32 +1,27 @@
-<<<<<<< HEAD
-# import sys
-# input = sys.stdin.readline
-
-
-N, K = map(int, input().split())
-coin_list = []
-cost_coin = 0
-
-for i in range(N):
-    coin_list.append(int(input()))
-
-coin_list.reverse()
-
-for j in coin_list:
-    if K - j > 0:                # =이 빠져서 ...! 빠져서 ...! 틀렸던 거 였어!! 그리고 이걸 캐치못해서 계속 헤맸다니... 
-        cost_coin += K // j
-        K = K % j
-    if K == 0:
-        break
-
-print(cost_coin)         # 이래도 안된다. 놓친게 있는듯
-=======
-"""
+m = int(input())
 n = int(input())
-cnt = 0
+prime_list = []
 
-while n > 3:
-    if n % 5 != 0 & n % 3 != 0:
-        if (n%10) % 3 == 0:
-"""
->>>>>>> 891a36352469225c1d3d4455f0cf76f898b11ecd
+if m <= 2:
+    if n != 1:
+        prime_list.append(2)
+
+for i in range(m, n+1):
+    if i <= 1:
+        continue
+    else:
+        for j in range(2, i):
+            if i % j == 0:
+                break
+            else:
+                if j == i-1:
+                    prime_list.append(i)
+
+if len(prime_list) == 0:
+    print(-1)
+else:
+    sum_prime = 0
+    for i in prime_list:
+        sum_prime += i
+    print(sum_prime)
+    print(prime_list[0])
