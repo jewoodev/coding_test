4f6032eae0a013b63ea131e0236cad4f1292af28
jewoodev <jewoos15@naver.com>
2024-03-08 21:49:58 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 136 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
@@ -2,22 +2,21 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	
-		long n = Long.parseLong(br.readLine());
-		long pi = n;
-		for(long i=2; i*i<=n; i++) {
-			if(n%i==0) {
-				pi = pi/i*(i-1);
-			}
-			while(n%i==0) {
-				n/=i;
-			}
-			
-		}
-		if(n!=1) {
-			pi = pi/n*(n-1);
-		}
-		System.out.println(pi);
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        long pi = n;
+        for (long i = 2; i * i <= n; i++) {
+            if(n % i == 0) {
+                pi = pi - pi / i;
+            }
+            while(n % i == 0) {
+                n /= i;
+            }
+
+        }
+        if (n != 1) {
+            pi = pi - pi / n;
+        }
+        System.out.println(pi);
+    }
 }
