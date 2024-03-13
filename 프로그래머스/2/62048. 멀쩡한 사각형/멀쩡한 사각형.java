dc24102f754a5061e9edf85a6b537a51fa48d345
jewoodev <jewoos15@naver.com>
2024-03-13 20:10:21 +0900

    Update 멀쩡한 사각형.java

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -1,38 +1,17 @@
-class Solution
-{
-	/**
-	 * 해답 반환 메서드
-	 *
-	 * @param w: [int] 가로 길이
-	 * @param h: [int] 세로 길이
-	 *
-	 * @return [int] 해답
-	 */
-	public long solution(int w, int h)
-	{
-		long ref = gcd(w, h);
-		
-		return ((long) w * h) - (((w / ref) + (h / ref) - 1) * ref);
-	}
+class Solution {
+    public long solution(int w, int h) {
+        /* 정사각형이 하나의 1차원 그래프라고 보고 주어진 조건대로의 대각선이 지나는 정수 단위의 좌표를 보면
+        너비와 높이를 너비와 높이의 최대공약수로 나눈 값의 배수로 규칙성을 갖는 것을 알 수 있다.
+        그렇기 때문에 단위정사각형의 개수는 최대공약수와 같게 되고, 그 크기는
+        ((w / gcd) * ((h / gcd) - 1) 의 규칙성을 갖는 것을 알 수 있다.  */
+        long gcd = gcd(w, h);
+        long answer = ((long) w * h) - ((w / gcd) * ((h / gcd) - 1) * gcd);
+        return answer;
+    }
 	
-	/**
-	 * 유클리드 호제법 연산결과 반환 메서드
-	 *
-	 * @param n: [int] 정수 1
-	 * @param m: [int] 정수 2
-	 *
-	 * @return [int] 최대공약수
-	 */
-	private int gcd(int n, int m)
-	{
-		while (m != 0)
-		{
-			int r = n % m;
-			
-			n = m;
-			m = r;
-		}
-		
-		return n;
-	}
+    //유클리드 호제법
+    private int gcd(int x, int y) {
+        if (x % y == 0) return y;
+        return gcd(y, x % y);
+    }
 }
