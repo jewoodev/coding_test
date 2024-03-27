23ec463cc2b537e62e8ba8a3f47351d43a8956ef
jewoodev <jewoos15@naver.com>
2024-03-27 13:43:45 +0900

    [Bronze I] Title: 2007년, Time: 212 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        String[] dow = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+        int[] months = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+        int day = 0;
+        for (int i = 0; i < x; i++) {
+            day += months[i];
+        }
+        day += y - 1;
+        System.out.print(dow[day%7]);
+    }
+}
