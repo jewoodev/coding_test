99bb785ce9d4d5b507a82333a7888b3a2099d784
jewoodev <jewoos15@naver.com>
2024-03-29 01:08:43 +0900

    [Silver V] Title: 끝말잇기, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/28432. 끝말잇기/끝말잇기.java
+++ 백준/Silver/28432. 끝말잇기/끝말잇기.java
@@ -4,39 +4,40 @@ import java.util.*;
 public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Set<String> previous = new HashSet<String>();
-        int N = Integer.parseInt(br.readLine());
+        Set<String> set = new HashSet<String>();
+        int n = Integer.parseInt(br.readLine());
         int idx = -1;
-        String[] arr = new String[N];
-        for(int i = 0; i < N; i++){
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
             String word = br.readLine();
-            if(!"?".equals(word)) previous.add(word);
+            if(!"?".equals(word)) set.add(word);
             else idx = i;
             arr[i] = word;
         }
-        int M = Integer.parseInt(br.readLine());
-        for(int i = 0; i < M; i++){
+        int m = Integer.parseInt(br.readLine());
+        for (int i = 0; i < m; i++) {
             String word = br.readLine();
-            if(N == 1) {
+            if (n == 1) {
                 System.out.println(word);
                 break;
-            };
-            if(previous.contains(word)) continue;
-            if(idx == 0){
-                if(arr[idx + 1].charAt(0) == word.charAt(word.length() - 1)){
+            }
+            if (set.contains(word)) continue;
+            if (idx == 0) {
+                if (arr[idx + 1].charAt(0) == word.charAt(word.length() - 1)) {
                     System.out.println(word);
                     break;
                 }
             }
-            else if(idx == N - 1){
-                if(arr[idx - 1].charAt(arr[idx - 1].length() - 1) == word.charAt(0)){
+            else if (idx == n - 1) {
+                String prev = arr[idx - 1];
+                if (prev.charAt(prev.length() - 1) == word.charAt(0)) {
                     System.out.println(word);
                     break;
                 }
             }
-            else{
-                if(arr[idx + 1].charAt(0) == word.charAt(word.length() - 1)
-                        && arr[idx - 1].charAt(arr[idx - 1].length() - 1) == word.charAt(0)){
+            else {
+                if (arr[idx + 1].charAt(0) == word.charAt(word.length() - 1)
+                        && arr[idx - 1].charAt(arr[idx - 1].length() - 1) == word.charAt(0)) {
                     System.out.println(word);
                     break;
                 }
