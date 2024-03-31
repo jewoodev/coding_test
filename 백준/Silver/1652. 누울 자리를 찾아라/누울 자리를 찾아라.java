1b9e23604aac88a2427d1d3e4b29b9f7d56dc113
jewoodev <jewoos15@naver.com>
2024-04-01 00:44:27 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 152 ms, Memory: 16064 KB -BaekjoonHub

---
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int horizon = 0; // 가로 개수
+        int vertical = 0; // 세로 개수
+        int N = Integer.parseInt(br.readLine());
+        char[][] room = new char[N][N];
+        for (int i = 0; i < N; i++) {
+            room[i] = br.readLine().toCharArray();
+        }
+        // 가로, 세로 방향으로 누울 수 있는지 여부
+        for (int i = 0; i < N; i++) {
+            int check_h = 0, check_v = 0;
+            for (int j = 0; j < N; j++) {
+                // 가로 체크
+                if (room[i][j] == '.') check_h++;
+                if (room[i][j] == 'X' || (j == N - 1)) {
+                    if (check_h >= 2) horizon++;
+                    check_h = 0;
+                }
+                // 세로 체크
+                if (room[j][i] == '.') check_v++;
+                if (room[j][i] == 'X' || (j == N - 1)) {
+                    if (check_v >= 2) vertical++;
+                    check_v = 0;
+                }
+            }
+        }
+        System.out.print(horizon + " " + vertical);
+    }
+}
