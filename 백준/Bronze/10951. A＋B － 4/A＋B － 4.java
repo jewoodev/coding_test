cff3a0b6e17503988b3ad7a000eb6dc55fd8178f
jewoodev <jewoos15@naver.com>
2024-04-01 19:53:12 +0900

    [Bronze V] Title: A+B - 4, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            try {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                sb.append(a + b).append("\n");
+            } catch (Exception e) {
+                break;
+            }
+        }
+        System.out.print(sb);
+    }
+}
