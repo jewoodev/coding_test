a4e5635a71d1509755da2fb6f2c8d93047d87b5f
jewoodev <jewoos15@naver.com>
2024-04-12 16:51:21 +0900

    Update 파티.java

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,14 +1,10 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
     private static int N, M, X, answer;
     private static List<Load>[] loads, reverse;
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static int[] dR = {-1, 1, 0, 0};
-    private static int[] dC = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
