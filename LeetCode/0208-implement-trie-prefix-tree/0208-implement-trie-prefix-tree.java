9ee0c15141d67f6859b5773b036cbdb1646c1dae
jewoodev <jewoos15@naver.com>
2024-11-17 10:17:09 +0900

    Time: 326 ms (5.02%), Space: 52.7 MB (98.20%) - LeetHub

---
+++ LeetCode/0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Trie {
+
+    private static List<String> list;
+    public Trie() {
+        list = new ArrayList();
+    }
+
+    public void insert(String word) {
+        list.add(word);
+    }
+
+    public boolean search(String word) {
+        return list.contains(word);
+    }
+
+    public boolean startsWith(String prefix) {
+        for (String s : list) {
+            if (s.startsWith(prefix)) return true;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
